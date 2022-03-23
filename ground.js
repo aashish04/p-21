601a56fa46ffc791e8239799ba4c59a044bbe0cb
aashish04 <95473698+aashish04@users.noreply.github.com>
2022-03-23 19:34:12 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,30 @@
+class Ground {
+
+    constructor(x,y,w,h){
+      let ground_options ={
+        isStatic:true
+      }
+      this.x=x;
+      this.y=y;
+      this.w = w;
+      this.h = h;
+      this.body = Bodies.rectangle(x,y,w,h,ground_options);
+      World.add(world,this.body);
+    }
+  
+    show(){
+      var pos = this.body.position;
+      push()
+      translate(pos.x,pos.y)
+      rectMode(CENTER);
+      stroke(255);
+      strokeWeight(4);
+      fill(255,255,0);
+      rect(0,0,this.w,this.h);
+      pop()
+  
+    }
+  }
+  
+  
+  
